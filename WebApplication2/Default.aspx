c3df93c3cb7e69e1e4fdc9f2fffb45b8c1952f19
Elji Varughese <Elji.Varughese@infotools.com>
2017-07-30 16:27:12 +1200

    Add project files.

---
+++ WebApplication2/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication2._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <p>
+        Saving Customer Details<br />
+    </p>
+    <p>
+        First Namae<asp:TextBox ID="firstName" runat="server" ToolTip="Enter your FN"></asp:TextBox>
+    </p>
+    <p>
+        Last Name<asp:TextBox ID="lastName" runat="server" OnTextChanged="lastName_TextChanged"></asp:TextBox>
+    </p>
+    <p>
+        Accound Number<asp:TextBox ID="accountNumber" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Save" Width="100px" />
+&nbsp;&nbsp;&nbsp;
+    </p>
+    <p>
+    </p>
+    <p>
+        &nbsp;</p>
+
+</asp:Content>
---
+++ WebApplication2/Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebApplication2
+{
+   public partial class _Default : Page
+   {
+      protected void Page_Load(object sender, EventArgs e)
+      {
+
+      }
+
+      protected void Button1_Click(object sender, EventArgs e)
+      {
+         SqlConnection conn = new SqlConnection(@"Server=WINDOWS-2AVHH68\SQL2012;Initial Catalog=CustomerDetails;uid=Test;pwd=Elji771799");
+         SqlCommand insert = new SqlCommand("insert into CustomerDetails(firstName, lastName, accountNumber) values(@firstName,@lastName,@accountNumber)", conn);
+         insert.Parameters.AddWithValue("@firstName", firstName.Text);
+         insert.Parameters.AddWithValue("@lastName", lastName.Text);
+         insert.Parameters.AddWithValue("@accountNumber", accountNumber.Text);
+
+         try
+         {
+            conn.Open();
+            insert.ExecuteNonQuery();
+            MessageBox.Show("Details Saved");
+         }
+         catch
+         {
+            conn.Close();
+            MessageBox.Show("Error in saving");
+         }
+      }
+
+      protected void lastName_TextChanged(object sender, EventArgs e)
+      {
+
+      }
+   }
+}
